ff844719057e3c5cd9ae324c953bac6603e9d081
Makson <xmonomnenado@gmail.com>
2024-08-16 22:17:38 +0300

    написав код який визначає слова полідроми та код який виводить у зворотньому порядку парні числа з масиву

--- builtIn.js
+++ builtIn.js
@@ -1,10 +1,36 @@
-function revstr(word){
-    return word.split('').reverse().join('')
-}
+  function revstr(word){
+      return word.split('').reverse().join('')
+  }
 
-let word='programist'
-let reverse=revstr(word)
+  let word='mandarinka'
+  let reverse=revstr(word)
 
-console.log(reverse)
+  console.log(reverse)
 
  
+
+
+  function ispolidrom(word){
+    
+     return word==word.split('').reverse().join('')
+  }
+
+  if(ispolidrom('121')){
+     console.log(true)
+  } else {
+     console.log(false)
+  }
+
+ 
+
+ const arr = [1,2,3,4,5,6]
+ const arr2 = []
+
+ for (const number of arr) {
+     if (number %2==0) {
+         arr2.push(number)
+     }
+ }
+ arr2.reverse()
+ console.log(arr2)
+
